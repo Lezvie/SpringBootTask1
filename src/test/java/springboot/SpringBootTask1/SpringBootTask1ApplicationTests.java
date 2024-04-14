97976e654e244d5e04fe454f19fe5c8f9d859f97
Lezvie <steklovrazor@yandex.ru>
2024-04-14 22:40:34 +0500

    Initial commit

---
+++ src/test/java/springboot/SpringBootTask1/SpringBootTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.SpringBootTask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
