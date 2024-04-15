edf8dbaf42b7b9263e4ae02d5ce1a741da92b076
Lezvie <steklovrazor@yandex.ru>
2024-04-15 18:24:18 +0500

    SpringBootTask

---
+++ src/main/java/springboot/SpringBootTask1/dao/UserDaoImpl.java
@@ -0,0 +1,39 @@
+package springboot.SpringBootTask1.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import springboot.SpringBootTask1.model.User;
+
+import java.util.List;
+
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+    public void createUser(User user) {
+        entityManager.persist(user);
+    }
+
+    public void deleteUser(Long id) {
+        entityManager.createQuery("delete from User user where user.id = :id").setParameter("id",id).executeUpdate();
+    }
+
+    public void updateUser(Long id, User user) {
+        User user1 = getUserById(id);
+        user1.setFirstName(user.getFirstName());
+        user1.setLastName(user.getLastName());
+        user1.setAge(user.getAge());
+        entityManager.merge(user1);
+    }
+
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User").getResultList();
+    }
+}
